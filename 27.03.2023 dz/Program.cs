7f8a0826f2db3f0face814ecb9adf22383d601fe
Leshamir <anastasy34@gmail.com>
2023-06-11 17:25:59 +0300

    Добавьте файлы проекта.

---
+++ 27.03.2023 dz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._03._2023_dz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
